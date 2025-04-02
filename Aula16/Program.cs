9c56b3c974cb0a9590a6af6782e32c73aa3d082a
danielfidelizar <carlos.paula@fidelizarmais.com>
2025-04-02 20:38:38 -0300

    Aprendendo Array

---
+++ Aula16/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Aula16;
+
+public class Program
+{
+    public static void Main()
+    {
+        string separator = "------------------";
+
+        //1. Declaração de Array
+        string[] fruits = { "Maçã, ", "Banana", "Caju", "Manga" };
+
+        //2. Acessar os valores
+        Console.WriteLine(fruits[1]);
+        Console.WriteLine(separator);
+
+        foreach (string fruit in fruits)
+        {
+            Console.WriteLine(fruit);
+        }
+        Console.WriteLine(separator);
+
+        //3. Contagem de Indices
+        Console.WriteLine("Quantidade de Índices: " + fruits.Length);
+
+        //4. Alterar os Valores
+        fruits[1] = "Abacaxi";
+        Console.WriteLine(fruits[1]);
+
+       
+    }
+}
