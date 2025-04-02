769f862c82d71ceff9977079bcaf3a343ae4140c
danielfidelizar <carlos.paula@fidelizarmais.com>
2025-04-02 01:00:43 -0300

    Aprendendo Foreach

---
+++ Aula15/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Aula15;
+
+public class Program
+{
+    public static void Main()
+    {
+        //EX1:
+
+        //Coleções (listas)
+
+        List<int> list = new List<int>()
+        { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+        ;
+        foreach (int i in list)
+        {
+
+            Console.WriteLine(i);
+        }
+
+    }
+}
