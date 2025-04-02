ffbefb6a8e8b8d53ad011d0968a57b24b0b710ad
danielfidelizar <carlos.paula@fidelizarmais.com>
2025-04-01 22:28:03 -0300

    Finalizando aula09

--- Aula08/Program.cs
+++ Aula08/Program.cs
@@ -5,11 +5,11 @@ public class Program
     public static void Main()
     {
         Console.WriteLine("====== Operadores Relacionais ======");
-        Console.WriteLine("Digite o primeiro numero: ");
+        Console.WriteLine("Digite o primeiro numero: (x) ");
 
         int x = Convert.ToInt32 (Console.ReadLine());
 
-        Console.WriteLine("Digite o segundo numero: ");
+        Console.WriteLine("Digite o segundo numero: (y) ");
         int y = Convert.ToInt32 (Console.ReadLine());
 
         string result;
@@ -25,5 +25,63 @@ public class Program
             result = "Os valores são Diferentes";
         }
         Console.WriteLine("\nIgualdade (==) : " + result);
+
+        //Operador de Diferença (!=)
+
+        if (x != y)
+        {
+            result = "x é diferente de y";
+        }
+        else
+        {
+            result = "x é igual a y";
+        }
+        Console.WriteLine("\nDiferença (!=) : " + result);
+
+        //Operador de Maior (>)
+
+        if(x > y)
+        {
+            result = "x é maior que y";
+        } else
+        {
+            result = "x não é maior que y";
+        }
+
+        Console.WriteLine("\nMaior que (>) : " + result);
+
+        //Operador Menor (<)
+
+        if (x < y)
+        {
+            result = "x é menor que y";
+        } else
+        {
+            result = "x não é menor que y";
+        }
+        Console.WriteLine("\nMenor que (<) : " + result);
+
+        //Operador Maior ou Igual (>=)
+
+        if (x >= y)
+        {
+            result = "x é maior ou igual a y";
+
+        }else
+        {
+            result = "x não é maior ou igual a y";
+        }
+        Console.WriteLine("\nMaior ou igual que (>=) : " + result);
+
+        //Operador Menor ou igual (<=)
+        if (x <= y)
+        {
+            result = "x é menor ou igual a y";
+        }else
+        {
+            result = "x não é menor ou igual a y";
+        }
+        Console.WriteLine("\nMenor ou igual que (<=) : " + result);
+
     }
 }
---
+++ Aula09/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Aula09;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("====== Operadores Lógicos ======");
+
+        bool isLogged = true;
+        bool hasAdminAcces = false;
+
+        Console.WriteLine("\nINformações do Usuário:");
+        Console.WriteLine("Usuário logado: " + isLogged);
+        Console.WriteLine("Acesso ao painel de administrador: " + hasAdminAcces);
+
+        //Operador (||) - Ou
+
+        if (isLogged || hasAdminAcces)
+        {
+            Console.WriteLine("Acesso ao sistema concedido!");
+        }
+        else
+        {
+            Console.WriteLine("Acesso ao sistema negado!");
+        }
+
+        //Operador (&&)
+
+        if (isLogged && hasAdminAcces)
+        {
+            Console.WriteLine("Acesso ao painel de administrador concedido!");
+        }
+        else
+        {
+            Console.WriteLine("Acesso ao painel administrador negado!");
+        }
+
+        //Operador de Negação (!) "não..."
+
+        if (!isLogged)
+        {
+            Console.WriteLine("Usuário não está logado!");
+        }
+        else
+        {
+            Console.WriteLine("Usuário está logado!");    
+        }
+
+
+
+
+    }
+}
