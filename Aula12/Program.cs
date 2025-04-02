ebb80041e20c714028bc4b984e9caa76296d9262
danielfidelizar <carlos.paula@fidelizarmais.com>
2025-04-02 00:38:05 -0300

    Aprendendo While

---
+++ Aula12/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Aula12;
+
+public class Program
+{
+    public static void Main()
+    {
+        int i = 0;
+        while (i <= 100)
+        {
+            Console.WriteLine(i);
+            i++;
+        }
+    }
+}
